d69b1b671073f4edab2a55f96bdff24205d58a45
TheFatAndFurious <mrguerrilla@gmail.com>
2024-11-21 22:00:51 +0100

    DAO interface

--- src/main/java/DB/GenericDAO.java
+++ src/main/java/DB/GenericDAO.java
@@ -1,5 +1,12 @@
 package DB;
 
-public interface GenericDAO {
-    void save(T entity)
+import java.sql.SQLException;
+import java.util.List;
+
+public interface GenericDAO<T, ID> {
+    void save(T entity) throws SQLException;
+    void delete(ID id) throws SQLException;
+    void update(ID id) throws SQLException;
+    List<T> getAll() throws SQLException;
+    T getById(ID id) throws SQLException;
 }
