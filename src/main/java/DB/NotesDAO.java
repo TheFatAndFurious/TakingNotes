84a27f4e4f78aea6a84d1e898e1bb71c26bf9697
TheFatAndFurious <mrguerrilla@gmail.com>
2024-11-21 22:40:07 +0100

    Notes DAO

--- src/main/java/DB/NotesDAO.java
+++ src/main/java/DB/NotesDAO.java
@@ -1,5 +1,10 @@
 package DB;
 
+import Entities.NotesEntity;
+
+import java.sql.*;
+import java.util.List;
+
 /**
  * Data access layer for the Notes, we should be able to:
  *  - create
@@ -8,12 +13,47 @@ package DB;
  *  - update
  *
  */
-public class NotesDAO {
-    DatabaseInit database;
+public class NotesDAO implements GenericDAO<NotesEntity, Long> {
+    Connection connection;
 
-    public NotesDAO(DatabaseInit database){
-        this.database = database;
+    public NotesDAO(Connection connection){
+        this.connection = connection;
     }
 
 
+    @Override
+    public void save(NotesEntity entity) throws SQLException {
+        String sqlStatement = "INSERT INTO notes (content) VALUES (?)";
+        try(PreparedStatement statement = connection.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS)){
+            statement.setString(1, entity.getContent());
+            statement.executeUpdate();
+            try (ResultSet keys = statement.getGeneratedKeys()){
+                if(keys.next()){
+                    entity.setID(keys.getLong(1));
+                }
+            }
+        } catch(SQLException e) {
+            throw new SQLException("Error registering the note", e);
+        }
+    }
+
+    @Override
+    public void delete(Long aLong) throws SQLException {
+
+    }
+
+    @Override
+    public void update(Long aLong) throws SQLException {
+
+    }
+
+    @Override
+    public List<NotesEntity> getAll() throws SQLException {
+        return List.of();
+    }
+
+    @Override
+    public NotesEntity getById(Long aLong) throws SQLException {
+        return null;
+    }
 }
--- src/main/java/Entities/NotesEntity.java
+++ src/main/java/Entities/NotesEntity.java
@@ -3,8 +3,16 @@ package Entities;
 import java.sql.Timestamp;
 
 public class NotesEntity {
-    int ID;
+    Long ID;
     String content;
     Timestamp timestamp;
     KeywordEntity keyword;
+
+    public String getContent(){
+        return this.content;
+    }
+
+    public void setID(Long id){
+        this.ID = id;
+    }
 }
