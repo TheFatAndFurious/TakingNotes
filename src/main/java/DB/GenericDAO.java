364d73d6cfc0aaef72b5a7c19395f45a22e0a0ba
TheFatAndFurious <mrguerrilla@gmail.com>
2024-11-22 11:29:39 +0100

    NotesDAO: Refactoring the save method for better testing

--- src/main/java/DB/GenericDAO.java
+++ src/main/java/DB/GenericDAO.java
@@ -4,7 +4,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 public interface GenericDAO<T, ID> {
-    void save(T entity) throws SQLException;
+    T save(T entity) throws SQLException;
     void delete(ID id) throws SQLException;
     void update(T identity, ID id) throws SQLException;
     List<T> getAll() throws SQLException;
--- src/main/java/DB/NotesDAO.java
+++ src/main/java/DB/NotesDAO.java
@@ -22,28 +22,30 @@ public class NotesDAO implements GenericDAO<NotesEntity, Long> {
     }
 
 
-    /**
-     * Saving a note to the database
-     * @param entity
-     * @throws SQLException
-     */
+
     @Override
-    public void save(NotesEntity entity) throws SQLException {
+    public NotesEntity save(NotesEntity entity) throws SQLException {
         String sqlStatement = "INSERT INTO notes (content) VALUES (?)";
         try(PreparedStatement statement = connection.prepareStatement(sqlStatement, Statement.RETURN_GENERATED_KEYS)){
             statement.setString(1, entity.getContent());
-            statement.executeUpdate();
+            int affectedRows = statement.executeUpdate();
+            if(affectedRows == 0) {
+                throw new SQLException("Failure to save note: no rows have been affected");
+            }
             try (ResultSet keys = statement.getGeneratedKeys()){
                 if(keys.next()){
                     entity.setID(keys.getLong(1));
+                } else {
+                    throw new SQLException("Failure to save note, no ID have been generated");
                 }
+            return entity;
             }
         } catch(SQLException e) {
             throw new SQLException("Error registering the note", e);
         }
     }
-
     // NOTE: Since this delete() will be used by all entities in the same way, should we create an abstract class that will implements our DAO interface and write a common delete() method ?
+
     /**
      * Method we will use to delete a note from the database
      * @param id which is the note's ID
