87e59556cbfdf4961158632c5b7251ff9bb1b65e
TheFatAndFurious <mrguerrilla@gmail.com>
2024-11-24 21:58:07 +0100

    playing with args in main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,17 @@
+import Entities.NotesEntity;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("ta soeur");
+        if(Objects.equals(args[0], "notes")){
+            System.out.println("we so good");
+        } else {
+            System.out.println("its a miss");
+        }
     }
 }
